e988cf4323e27e710e6c09b9cbb623116106996c
Fadgiras <mayeuxgudinaflorian@gmail.com>
2020-10-26 01:07:33 +0100

    Wrapped NumberOfpaths to avoid a static error, test 1 passed, still to fix

--- src/Projet/Solution.java
+++ src/Projet/Solution.java
@@ -4,51 +4,31 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-class Solution {
+class Solution{
 
-    static int numberOfPaths(int m, int n, HashMap<String, Boolean> map)
-    {
-        int x = 0;
-        int y = 0;
-        //If start or end is wall then 0 paths
-        if(isWall(0, 0, map) || isWall(m-1, n-1, map))
-            return 0;
-        //If map explored stop
-        if(wallKey(x,y)==wallKey(m, n))
-            return 0;
-        //if current cell is wall one path minus
-        if(isWall(x, y, map))
-            return 0;
-        // base algorithm
-        if (m == 1 || n == 1)
-            return 1;
-        //return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1);
-        return numberOfPaths(x + 1, y, map) + numberOfPaths(x, y + 1, map);
-    }
-
-    static String wallKey(int x, int y){
+    static String wallKey(final int x, final int y){
         return x + "," + y;
     }
 
-    static Boolean isThereWall(int x, String Row){
+    static Boolean isThereWall(final int x, final String Row){
         return Row.charAt(x) == '1';
     }
 
-    static Boolean isWall(int x, int y, HashMap<String, Boolean> map){
+    static Boolean isWall(final int x, final int y, final HashMap<String, Boolean> map){
         return map.get(wallKey(x, y));
     }
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int M = in.nextInt();
-        int N = in.nextInt();
+    public static void main(final String args[]) {
+        final Scanner in = new Scanner(System.in);
+        final int M = in.nextInt();
+        final int N = in.nextInt();
         if (in.hasNextLine()) {
             in.nextLine();
         }
         //Filling the map
-        HashMap Walls = new HashMap<String, Boolean>();
+        final HashMap Walls = new HashMap<String, Boolean>();
         for (int y = 0; y < M; y++) {
-            String ROW = in.nextLine();
+            final String ROW = in.nextLine();
             for (int x = 0; x < N; x++) {
                 Walls.put(wallKey(x, y), isThereWall(x, ROW));
             }
@@ -64,6 +44,39 @@ class Solution {
             }
         }
         */
-        System.out.println(numberOfPaths(M, N, Walls));
+        in.close();
+        pathFinder toPF = new pathFinder(M, N, Walls);
+        System.out.println(toPF.numberOfPaths(M, N, Walls));
+    }
+}
+
+//to avoid the "Static error"
+class pathFinder{
+
+    private final HashMap<String, Boolean> map;
+    private final int m, n;
+    public pathFinder(final int m, final int n, final HashMap<String, Boolean> map) {
+        this.map = map;
+        this.m = m;
+        this.n = m;
+    }
+    public int numberOfPaths(final int m, final int n, final HashMap<String, Boolean> map)
+    {
+        final int x = 0;
+        final int y = 0;
+        //If start or end is wall then 0 paths
+        if(Solution.isWall(0, 0, map) || Solution.isWall(m-1, n-1, map))
+            return 0;
+        //If map explored stop
+        if(Solution.wallKey(x,y)==Solution.wallKey(m, n))
+            return 0;
+        //if current cell is wall one path minus
+        if(Solution.isWall(x, y, map))
+            return 0;
+        // base algorithm
+        if (m == 1 || n == 1)
+            return 1;
+        //return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1);
+        return numberOfPaths(x + 1, y, map) + numberOfPaths(x, y + 1, map);
     }
 }
